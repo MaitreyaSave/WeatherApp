25fc2dc740de3b1e3201c727517912442f062684
Maitreya Save <msave@ncsu.edu>
2021-09-05 16:04:51 -0400

    - Basic UI components of Search box and search button added.

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,11 +1,166 @@
 package com.example.weatherapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
+import com.example.weatherapp.ui.components.TextSearchBar
+import com.example.weatherapp.ui.components.autocomplete.AutoCompleteBox
+import com.example.weatherapp.ui.components.autocomplete.utils.AutoCompleteSearchBarTag
+import com.example.weatherapp.ui.components.autocomplete.utils.asAutoCompleteEntities
+import com.example.weatherapp.ui.theme.WeatherAppTheme
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : ComponentActivity() {
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            WeatherAppTheme {
+                Scaffold(
+                    topBar = { TopBar(
+                        backgroundColor = MaterialTheme.colors.primaryVariant
+                    ) },
+                    backgroundColor = Color.Gray
+                ) {
+
+                }
+            }
+        }
     }
 }
+//
+
+@ExperimentalAnimationApi
+@Composable
+fun TopBar(backgroundColor: Color) {
+    val items = listOf(
+        "New York",
+        "New Jersey",
+        "Raleigh",
+        "Cary",
+        "Morrisville",
+        "Durham"
+    )
+
+    // TopBar Row
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(backgroundColor)
+
+    ) {
+
+        // Inner Row
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+                .background(Color.White)
+                .padding(0.dp, 0.dp, 0.dp, 8.dp)
+
+        ) {
+
+            // Search Icon Button
+            IconButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .padding(8.dp, 12.dp, 8.dp, 0.dp)
+
+            ) {
+                Icon(
+                    Icons.Filled.Search,
+                    contentDescription = "Search Icon",
+                    tint = Color.Gray,
+                    modifier = Modifier.size(32.dp)
+                )
+            }
+
+            // Search Box
+            AutoCompleteValue(items)
+        }
+    }
+
+}
+
+
+@ExperimentalAnimationApi
+@Composable
+fun AutoCompleteValue(items: List<String>) {
+
+//    val items = listOf(
+//        "Paulo Pereira",
+//        "Daenerys Targaryen",
+//        "Jon Snow",
+//        "Sansa Stark",
+//    )
+    val autoCompleteEntities = items.asAutoCompleteEntities(
+        filter = { item, query ->
+            item.lowercase(Locale.getDefault())
+                .startsWith(query.lowercase(Locale.getDefault()))
+        }
+    )
+
+    AutoCompleteBox(
+        items = autoCompleteEntities,
+        itemContent = { item ->
+            ValueAutoCompleteItem(item.value)
+        }
+    ) {
+        var value by remember { mutableStateOf("") }
+        val view = LocalView.current
+
+        onItemSelected { item ->
+            value = item.value
+            filter(value)
+            view.clearFocus()
+        }
+
+        TextSearchBar(
+            modifier = Modifier.testTag(AutoCompleteSearchBarTag),
+            value = value,
+            label = "Search city",
+            onDoneActionClick = {
+                view.clearFocus()
+            },
+            onClearClick = {
+                value = ""
+                filter(value)
+                view.clearFocus()
+            },
+            onFocusChanged = { focusState ->
+                isSearching = focusState.hasFocus
+            },
+            onValueChanged = { query ->
+                value = query
+                filter(value)
+            }
+        )
+    }
+}
+
+@Composable
+fun ValueAutoCompleteItem(item: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(text = item, style = MaterialTheme.typography.subtitle2)
+    }
+}
+
+
---
+++ app/src/main/java/com/example/weatherapp/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.weatherapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+// Custom colors for weather app
+val PrimaryBlue = Color(0xFF2F3E9E)
